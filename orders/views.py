f27169d34cd80c435dec8fe7f4f9463647b2deee
LittleStupid <lttlstpd@live.com>
2017-01-15 14:55:44 +0800

    paypal

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from django.core.urlresolvers import reverse
+# from .tasks import order_created
 
 # Create your views here.
 
@@ -18,6 +20,7 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            # order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
